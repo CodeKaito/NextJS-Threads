2025960938d011281e855b0fe7a2c22135d3f91e
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-08-08 14:45:18 +0200

    update

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,5 +1,93 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { useOrganization } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+import { ThreadValidation } from "@/lib/validations/thread";
+import { createThread } from "@/lib/actions/thread.actions";
+
+interface Props {
+    user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+    };
+    btnTitle: string;
+}
+
+    
+
 function PostThread({ userId } : { userId: string }) {
-    return <h1>Post Thread Forum</h1>
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const form = useForm({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+        thread: '',
+        accountId: userId,
+    },
+    });
+
+    const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+        await createThread({
+            text: values.thread,
+            author: userId,
+            communityId: null,
+            path: pathname,
+        });
+    
+        router.push("/");
+    };
+
+    return (
+        <Form {...form}>
+            <form 
+            onSubmit={form.handleSubmit(onSubmit)} 
+            className="mt-10 flex flex-col justify-start gap-10">
+
+                <FormField
+                control={form.control}
+                name="thread"
+                render={({ field }) => (
+                <FormItem className="flex flex-col gap-3 w-full">
+                    <FormLabel className="text-base-semibold text-light-2">
+                        Content
+                    </FormLabel>
+                    <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                        <Textarea
+                        rows={15}
+                        {...field}
+                        placeholder="What's on your thread?"
+                        />
+                    </FormControl>
+
+                    <FormMessage />
+            </FormItem>
+                )}
+            />
+
+            <Button type='submit' className='bg-primary-500'>Publish</Button>
+            </form>
+        </Form>
+    )
 }
 
 export default PostThread;
---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,29 @@
+import { revalidatePath } from "next/cache";
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+
+interface Params {
+    text: string,
+    author: string,
+    communityId: string | null,
+    path: string,
+
+}
+
+export async function createThread({ text, author, communityId, path }: Params) {
+    connectToDB();
+
+    const createThread = await Thread.create({
+        text,
+        author,
+        community: null,
+    });
+
+    // update user model
+    await User.findByIdAndUpdate(author, {
+        $push: { threads: createThread._id }
+    })
+
+    revalidatePath(path)
+};

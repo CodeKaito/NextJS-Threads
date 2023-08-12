e58c60dbe2301f45262da60dc5aa3e17f5454ed7
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-08-12 12:34:34 +0200

    update

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -5,6 +5,7 @@ import ProfileHeader from "@/components/shared/ProfileHeader";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 import { profileTabs } from "@/constants";
+import { TabsContent } from "@radix-ui/react-tabs";
 
 async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
@@ -37,9 +38,24 @@ async function Page({ params }: { params: { id: string } }) {
                                     className="object-contains"
                                 />
                                 <p className="max-sm:hidden">{tab.label}</p>
+
+                                {tab.label === 'Threads' && (
+                                    <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                                        {userInfo?.threads?.length}
+                                    </p>
+                                )}
                             </TabsTrigger>
                         ))}
                     </TabsList>
+                    {profileTabs.map((tab) => (
+                        <TabsContent key={`content-${tab.label}`} value={tab.value} className="w-full text-light-1">
+                            <ThreadsTab
+                                currentUserId={user.id} 
+                                accountId={userInfo.id}
+                                accountType="User"
+                            />
+                        </TabsContent>
+                    ))}
                 </Tabs>
             </div>
         </section>

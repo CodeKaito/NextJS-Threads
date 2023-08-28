3e0dc432bcb9c34c66264dd4d0d3b86be013c822
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-08-28 11:32:19 +0200

    update

--- app/(root)/activity/dist/page.js
+++ app/(root)/activity/dist/page.js
@@ -36,10 +36,31 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 exports.__esModule = true;
-var Page = function () { return __awaiter(void 0, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        return [2 /*return*/, (React.createElement("section", null,
-                React.createElement("h1", { className: "head-text mb-10" }, "Activity")))];
+var nextjs_1 = require("@clerk/nextjs");
+var navigation_1 = require("next/navigation");
+var user_actions_1 = require("@/lib/actions/user.actions");
+function Page() {
+    return __awaiter(this, void 0, void 0, function () {
+        var user, userInfo, activity;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, nextjs_1.currentUser()];
+                case 1:
+                    user = _a.sent();
+                    if (!user)
+                        return [2 /*return*/, null];
+                    return [4 /*yield*/, user_actions_1.fetchUser(user.id)];
+                case 2:
+                    userInfo = _a.sent();
+                    if (!(userInfo === null || userInfo === void 0 ? void 0 : userInfo.onboarded))
+                        navigation_1.redirect("/onboarding");
+                    return [4 /*yield*/, getActivity(userInfo._id)];
+                case 3:
+                    activity = _a.sent();
+                    return [2 /*return*/, (React.createElement(React.Fragment, null,
+                            React.createElement("h1", { className: 'head-text' }, "Activity")))];
+            }
+        });
     });
-}); };
+}
 exports["default"] = Page;
--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,9 +1,23 @@
-const Page = async () => {
-    return (
-      <section>
-          <h1 className="head-text mb-10">Activity</h1>
-      </section>
-    )
-  }
-  
-  export default Page;
+import Image from "next/image";
+import Link from "next/link";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const activity = await getActivity(userInfo._id);
+
+  return (
+    <>
+      <h1 className='head-text'>Activity</h1>
+    </>
+  )
+}
+
+export default Page;
